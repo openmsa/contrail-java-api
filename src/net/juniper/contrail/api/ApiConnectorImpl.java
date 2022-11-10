345b8217913db4d554388d4d1f00750c7631b26e
Olivier Vignaud <ovi@ubiqube.com>
2022-11-10 10:02:06 +0100

    Fix OpenStack connection.

--- src/net/juniper/contrail/api/ApiConnectorImpl.java
+++ src/net/juniper/contrail/api/ApiConnectorImpl.java
@@ -29,11 +29,12 @@ import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
+import org.apache.http.client.protocol.RequestExpectContinue;
 import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.DefaultBHttpClientConnection;
 import org.apache.http.impl.DefaultConnectionReuseStrategy;
-import org.apache.http.impl.DefaultHttpClientConnection;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicHttpEntityEnclosingRequest;
@@ -42,15 +43,14 @@ import org.apache.http.params.HttpProtocolParams;
 import org.apache.http.params.SyncBasicHttpParams;
 import org.apache.http.protocol.BasicHttpContext;
 import org.apache.http.protocol.BasicHttpProcessor;
-import org.apache.http.protocol.ExecutionContext;
 import org.apache.http.protocol.HttpContext;
+import org.apache.http.protocol.HttpCoreContext;
 import org.apache.http.protocol.HttpProcessor;
 import org.apache.http.protocol.HttpRequestExecutor;
 import org.apache.http.protocol.ImmutableHttpProcessor;
 import org.apache.http.protocol.RequestConnControl;
 import org.apache.http.protocol.RequestContent;
 import org.apache.http.protocol.RequestDate;
-import org.apache.http.protocol.RequestExpectContinue;
 import org.apache.http.protocol.RequestTargetHost;
 import org.apache.http.protocol.RequestUserAgent;
 import org.apache.http.ssl.SSLContexts;
@@ -60,6 +60,7 @@ import org.openstack4j.api.OSClient.OSClientV2;
 import org.openstack4j.api.OSClient.OSClientV3;
 import org.openstack4j.api.client.IOSClientBuilder.V3;
 import org.openstack4j.api.exceptions.AuthenticationException;
+import org.openstack4j.core.transport.Config;
 import org.openstack4j.model.common.Identifier;
 import org.openstack4j.openstack.OSFactory;
 import org.slf4j.Logger;
@@ -74,36 +75,37 @@ class ApiConnectorImpl implements ApiConnector {
 
 	private static final Logger LOG = LoggerFactory.getLogger(ApiConnectorImpl.class);
 
-	private final String _api_hostname;
-	private final int _api_port;
-	private final ApiBuilder _apiBuilder;
+	private final String apiHostname;
+	private final int apiPort;
+	private final ApiBuilder apiBuilder;
 
 	private String _username;
 	private String _password;
-	private String _tenant;
-	private boolean _has_input_authtoken;
-	private String _authtoken;
-	private String _authtype;
-	private String _authurl;
+	private String tenantId;
+	private String domainId;
+	private boolean hasInputAuthtoken;
+	private String authToken;
+	private String authType;
+	private String authUrl;
 
 	// HTTP Connection parameters
 	private HttpParams _params;
-	private HttpProcessor _httpproc;
-	private HttpRequestExecutor _httpexecutor;
-	private HttpContext _httpcontext;
-	private HttpHost _httphost;
-	private DefaultHttpClientConnection _connection;
-	private ConnectionReuseStrategy _connectionStrategy;
-	public final static int MAX_RETRIES = 5;
+	private HttpProcessor httpProc;
+	private HttpRequestExecutor httpExecutor;
+	private HttpContext httpContext;
+	private HttpHost httpHost;
+	private DefaultBHttpClientConnection connection;
+	private ConnectionReuseStrategy connectionStrategy;
+	public static final int MAX_RETRIES = 5;
 	public final int clientId = 2;
 
 	public ApiConnectorImpl(final String hostname, final int port) {
-		_api_hostname = hostname;
-		_api_port = port;
-		_has_input_authtoken = true;
+		apiHostname = hostname;
+		apiPort = port;
+		hasInputAuthtoken = true;
 		initHttpClient();
 		initHttpServerParams(hostname, port);
-		_apiBuilder = new ApiBuilder();
+		apiBuilder = new ApiBuilder();
 	}
 
 	private void initHttpClient() {
@@ -113,7 +115,7 @@ class ApiConnectorImpl implements ApiConnector {
 		HttpProtocolParams.setUseExpectContinue(_params, false);
 		HttpProtocolParams.setHttpElementCharset(_params, "UTF-8");
 
-		_httpproc = new ImmutableHttpProcessor(
+		httpProc = new ImmutableHttpProcessor(
 				// Required protocol interceptors
 				new BasicHttpProcessor(),
 				new RequestConnControl(),
@@ -123,18 +125,16 @@ class ApiConnectorImpl implements ApiConnector {
 				// Recommended protocol interceptors
 				new RequestUserAgent(),
 				new RequestExpectContinue());
-		_httpexecutor = new HttpRequestExecutor();
-		_httpcontext = new BasicHttpContext(null);
-		_connection = new DefaultHttpClientConnection();
-		_connectionStrategy = new DefaultConnectionReuseStrategy();
+		httpExecutor = new HttpRequestExecutor();
+		httpContext = new BasicHttpContext(null);
+		connection = new DefaultBHttpClientConnection(8192);
+		connectionStrategy = new DefaultConnectionReuseStrategy();
 	}
 
 	private void initHttpServerParams(final String hostname, final int port) {
-
-		_httphost = new HttpHost(hostname, port, "https");
-		_httpcontext.setAttribute(ExecutionContext.HTTP_CONNECTION, _connection);
-		_httpcontext.setAttribute(ExecutionContext.HTTP_TARGET_HOST, _httphost);
-
+		httpHost = new HttpHost(hostname, port, "https");
+		httpContext.setAttribute(HttpCoreContext.HTTP_CONNECTION, connection);
+		httpContext.setAttribute(HttpCoreContext.HTTP_TARGET_HOST, httpHost);
 	}
 
 	@Override
@@ -145,41 +145,47 @@ class ApiConnectorImpl implements ApiConnector {
 	}
 
 	@Override
-	public ApiConnector tenantName(final String tenant) {
-		_tenant = tenant;
+	public ApiConnector domainName(final String tenant) {
+		domainId = tenant;
+		return this;
+	}
+
+	@Override
+	public ApiConnector tenantId(final String tenant) {
+		this.tenantId = tenant;
 		return this;
 	}
 
 	@Override
 	public ApiConnector authToken(final String token) {
-		_has_input_authtoken = true;
-		_authtoken = token;
+		hasInputAuthtoken = true;
+		authToken = token;
 		return this;
 	}
 
 	@Override
 	public ApiConnector authServer(final String type, final String url) {
-		_has_input_authtoken = false;
-		_authtype = type;
-		_authurl = url;
+		hasInputAuthtoken = false;
+		authType = type;
+		authUrl = url;
 		return this;
 	}
 
 	private void checkResponseKeepAliveStatus(final HttpResponse response) throws IOException {
 
-		if (!_connectionStrategy.keepAlive(response, _httpcontext)) {
-			_connection.close();
+		if (!connectionStrategy.keepAlive(response, httpContext)) {
+			connection.close();
 		}
 	}
 
 	private void checkConnection() throws IOException {
-		if (!_connection.isOpen()) {
-			LOG.info("http connection <{}, {}> does not exit", _httphost.getHostName(), _httphost.getPort());
+		if (!connection.isOpen()) {
+			LOG.info("http connection <{}, {}> does not exit", httpHost.getHostName(), httpHost.getPort());
 			// Socket is used by _connection.
 			@SuppressWarnings("resource")
-			final Socket socket = new Socket(_httphost.getHostName(), _httphost.getPort());
-			_connection.bind(socket, _params);
-			LOG.info("http connection <{}, {}> established", _httphost.getHostName(), _httphost.getPort());
+			final Socket socket = new Socket(httpHost.getHostName(), httpHost.getPort());
+			connection.bind(socket);
+			LOG.info("http connection <{}, {}> established", httpHost.getHostName(), httpHost.getPort());
 		}
 	}
 
@@ -189,44 +195,47 @@ class ApiConnectorImpl implements ApiConnector {
 	}
 
 	public String getHostName() {
-		return _api_hostname;
+		return apiHostname;
 	}
 
 	public int getPort() {
-		return _api_port;
+		return apiPort;
 	}
 
 	// return value indicates whether the token changed
 	private boolean authenticate() {
-		if (_has_input_authtoken) {
+		if (hasInputAuthtoken) {
 			return false;
 		}
 
-		_authtoken = null;
-		if ("keystone".equals(_authtype)) {
+		authToken = null;
+		if ("keystone".equals(authType)) {
 			try {
-				final OSClientV2 os = OSFactory.builderV2().endpoint(_authurl)
-						.credentials(_username, _password).tenantName(_tenant).authenticate();
-				_authtoken = os.getAccess().getToken().getId();
+				final OSClientV2 os = OSFactory.builderV2().endpoint(authUrl)
+						.credentials(_username, _password).tenantName(domainId).authenticate();
+				authToken = os.getAccess().getToken().getId();
 				return true;
 			} catch (final AuthenticationException authe) {
-				LOG.warn("authenticate to keystone {} failed: {}", authe, _authurl);
+				LOG.warn("authenticate to keystone {} failed: {}", authe, authUrl);
 			}
-		} else if ("keystonev3".equals(_authtype)) {
+		} else if ("keystonev3".equals(authType)) {
 			try {
-				final V3 base = OSFactory.builderV3().endpoint(_authurl);
-				base.useNonStrictSSLClient(true);
-				if (_tenant != null) {
-					final Identifier domainIdentifier = Identifier.byName(_tenant);
+				final V3 base = OSFactory.builderV3().endpoint(authUrl);
+				base.withConfig(Config.newConfig().withSSLVerificationDisabled());
+				if (domainId != null) {
+					final Identifier domainIdentifier = Identifier.byName(domainId);
 					base.credentials(_username, _password, domainIdentifier);
 				} else {
 					base.credentials(_username, _password);
 				}
+				if (null != tenantId) {
+					base.scopeToProject(Identifier.byId(tenantId));
+				}
 				final OSClientV3 os = base.authenticate();
-				_authtoken = os.getToken().getId();
+				authToken = os.getToken().getId();
 				return true;
 			} catch (final AuthenticationException authe) {
-				LOG.warn("authenticate to keystone {} failed: {}", authe, _authurl);
+				LOG.warn("authenticate to keystone {} failed: {}", authe, authUrl);
 			}
 		}
 
@@ -242,7 +251,7 @@ class ApiConnectorImpl implements ApiConnector {
 			final int retryCount) throws IOException {
 		int rc = retryCount;
 		checkConnection();
-		if ((_authtoken == null) && (_authtype != null)) {
+		if ((authToken == null) && (authType != null)) {
 			authenticate();
 		}
 		final BasicHttpEntityEnclosingRequest request = new BasicHttpEntityEnclosingRequest(method, uri);
@@ -253,8 +262,8 @@ class ApiConnectorImpl implements ApiConnector {
 			LOG.debug(">> Request: {}, {}", method, request.getRequestLine().getUri());
 		}
 		request.setParams(_params);
-		if (_authtoken != null) {
-			request.setHeader("X-AUTH-TOKEN", _authtoken);
+		if (authToken != null) {
+			request.setHeader("X-AUTH-TOKEN", authToken);
 		}
 		final SSLContext sslContext = createSslContext();
 		CloseableHttpResponse response;
@@ -262,9 +271,9 @@ class ApiConnectorImpl implements ApiConnector {
 				.setSSLContext(sslContext)
 				.setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE)
 				.build()) {
-			response = httpclient.execute(_httphost, request);
+			response = httpclient.execute(httpHost, request);
 			response.setParams(_params);
-			_httpexecutor.postProcess(response, _httpproc, _httpcontext);
+			httpExecutor.postProcess(response, httpProc, httpContext);
 		} catch (final Exception e) {
 			if (rc == 0) {
 				LOG.error("<< Received exception from the Api server, max retries exhausted: ", e);
@@ -403,7 +412,7 @@ class ApiConnectorImpl implements ApiConnector {
 
 	@Override
 	public synchronized Status create(final ApiObjectBase obj) throws IOException {
-		final String typename = _apiBuilder.getTypename(obj.getClass());
+		final String typename = ApiBuilder.getTypename(obj.getClass());
 		final String jsdata = ApiSerializer.serializeObject(typename, obj);
 
 		HttpResponse response;
@@ -433,7 +442,7 @@ class ApiConnectorImpl implements ApiConnector {
 			return Status.failure(reason);
 		}
 
-		final ApiObjectBase resp = _apiBuilder.jsonToApiObject(getResponseData(response), obj.getClass());
+		final ApiObjectBase resp = apiBuilder.jsonToApiObject(getResponseData(response), obj.getClass());
 		if (resp == null) {
 			final String reason = "Unable to decode Create response";
 			LOG.error(reason);
@@ -457,7 +466,7 @@ class ApiConnectorImpl implements ApiConnector {
 
 	@Override
 	public synchronized Status update(final ApiObjectBase obj) throws IOException {
-		final String typename = _apiBuilder.getTypename(obj.getClass());
+		final String typename = ApiBuilder.getTypename(obj.getClass());
 		final String jsdata = ApiSerializer.serializeObject(typename, obj);
 		final HttpResponse response = execute(HttpPut.METHOD_NAME, "/" + typename + '/' + obj.getUuid(),
 				new StringEntity(jsdata, ContentType.APPLICATION_JSON));
@@ -482,7 +491,7 @@ class ApiConnectorImpl implements ApiConnector {
 
 	@Override
 	public synchronized Status read(final ApiObjectBase obj) throws IOException {
-		final String typename = _apiBuilder.getTypename(obj.getClass());
+		final String typename = ApiBuilder.getTypename(obj.getClass());
 		final HttpResponse response = execute(HttpGet.METHOD_NAME,
 				"/" + typename + '/' + obj.getUuid(), null);
 
@@ -501,7 +510,7 @@ class ApiConnectorImpl implements ApiConnector {
 			return Status.failure(reason);
 		}
 		LOG.debug("Response: {}", response);
-		final ApiObjectBase resp = _apiBuilder.jsonToApiObject(getResponseData(response), obj.getClass());
+		final ApiObjectBase resp = apiBuilder.jsonToApiObject(getResponseData(response), obj.getClass());
 		if (resp == null) {
 			final String message = "Unable to decode GET response.";
 			LOG.warn(message);
@@ -525,7 +534,7 @@ class ApiConnectorImpl implements ApiConnector {
 			return Status.success();
 		}
 
-		final String typename = _apiBuilder.getTypename(cls);
+		final String typename = ApiBuilder.getTypename(cls);
 		final HttpResponse response = execute(HttpDelete.METHOD_NAME,
 				"/" + typename + '/' + uuid, null);
 
@@ -571,7 +580,7 @@ class ApiConnectorImpl implements ApiConnector {
 
 	@Override
 	public synchronized ApiObjectBase findById(final Class<? extends ApiObjectBase> cls, final String uuid) throws IOException {
-		final String typename = _apiBuilder.getTypename(cls);
+		final String typename = ApiBuilder.getTypename(cls);
 		final HttpResponse response = execute(HttpGet.METHOD_NAME,
 				'/' + typename + '/' + uuid, null);
 
@@ -584,7 +593,7 @@ class ApiConnectorImpl implements ApiConnector {
 			checkResponseKeepAliveStatus(response);
 			return null;
 		}
-		final ApiObjectBase object = _apiBuilder.jsonToApiObject(getResponseData(response), cls);
+		final ApiObjectBase object = apiBuilder.jsonToApiObject(getResponseData(response), cls);
 		if (object == null) {
 			LOG.warn("Unable to decode find response");
 		}
@@ -595,27 +604,27 @@ class ApiConnectorImpl implements ApiConnector {
 
 	@Override
 	public String findByName(final Class<? extends ApiObjectBase> cls, final ApiObjectBase parent, final String name) throws IOException {
-		final List<String> name_list = new ArrayList<>();
+		final List<String> nameList = new ArrayList<>();
 		if (parent != null) {
-			name_list.addAll(parent.getQualifiedName());
+			nameList.addAll(parent.getQualifiedName());
 		} else {
 			try {
-				name_list.addAll(cls.newInstance().getDefaultParent());
+				nameList.addAll(cls.newInstance().getDefaultParent());
 			} catch (final Exception ex) {
 				// Instantiation or IllegalAccess
 				LOG.error("Failed to instantiate object of class {}", cls.getName(), ex);
 				return null;
 			}
 		}
-		name_list.add(name);
-		return findByName(cls, name_list);
+		nameList.add(name);
+		return findByName(cls, nameList);
 	}
 
 	@Override
 	// POST http://hostname:port/fqname-to-id
 	// body: {"type": class, "fq_name": [parent..., name]}
 	public synchronized String findByName(final Class<? extends ApiObjectBase> cls, final List<String> name_list) throws IOException {
-		final String jsonStr = _apiBuilder.buildFqnJsonString(cls, name_list);
+		final String jsonStr = apiBuilder.buildFqnJsonString(cls, name_list);
 		final HttpResponse response = execute(HttpPost.METHOD_NAME, "/fqname-to-id",
 				new StringEntity(jsonStr, ContentType.APPLICATION_JSON));
 
@@ -636,7 +645,7 @@ class ApiConnectorImpl implements ApiConnector {
 		}
 		LOG.debug("<< Response Data: {}", data);
 
-		final String uuid = _apiBuilder.getUuid(data);
+		final String uuid = apiBuilder.getUuid(data);
 		if (uuid == null) {
 			LOG.warn("Unable to parse response");
 			checkResponseKeepAliveStatus(response);
@@ -648,7 +657,7 @@ class ApiConnectorImpl implements ApiConnector {
 
 	@Override
 	public synchronized List<? extends ApiObjectBase> list(final Class<? extends ApiObjectBase> cls, final List<String> parent) throws IOException {
-		final String typename = _apiBuilder.getTypename(cls);
+		final String typename = ApiBuilder.getTypename(cls);
 		final HttpResponse response = execute(HttpGet.METHOD_NAME, '/' + typename + 's', null);
 
 		if ((response == null) || (response.getStatusLine() == null)) {
@@ -667,7 +676,7 @@ class ApiConnectorImpl implements ApiConnector {
 			checkResponseKeepAliveStatus(response);
 			return null;
 		}
-		final List<? extends ApiObjectBase> list = _apiBuilder.jsonToApiObjects(data, cls, parent);
+		final List<? extends ApiObjectBase> list = apiBuilder.jsonToApiObjects(data, cls, parent);
 		if (list == null) {
 			LOG.warn("Unable to parse/deserialize response: {}", data);
 		}
@@ -721,8 +730,8 @@ class ApiConnectorImpl implements ApiConnector {
 	@Override
 	public void dispose() {
 		try {
-			if (_connection.isOpen()) {
-				_connection.close(); // close server connection
+			if (connection.isOpen()) {
+				connection.close(); // close server connection
 			}
 		} catch (final IOException ex) {
 			LOG.warn("Exception while closing server connection: {}", ex.getMessage());
